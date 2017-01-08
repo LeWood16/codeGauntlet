dcec39d44df83889d6dac95d36829d8bacd7371c
lewood <lewoodwebdev@gmail.com>
2017-01-07 22:20:07 -0500

    took solution out of test template, added it to test file

--- tests/test1.js
+++ tests/test1.js
@@ -1,34 +1,43 @@
-var testNums = [123, 2121, 5309];
-var testAnswers = [321, 1212, 9035];
-var userAnswers = [];
-
-// create a function that will take in a number as an argument, then returns that number in reverse
-// replace "testFunction" with the name of your function
-
-/* e.g. 
-function numReverse(num){
-   // some code;
-   return num;
-}
-
+/* Create a function that will take in 
+   a number as an argument, then returns 
+   that number in reverse. Replace the 
+   code in "challengeFunction" with your code.
+   Press "Run" when finished.
 */
 
-var youPassed = true;
+function challengeFunction(n){
+  // your code goes
 
-for (var i = 0; i < testNums.length; i++){
-    userAnswers.push("textFunction"(testNums[i]));
+
+  
+  // between these lines
 }
 
-for (var j = 0; j < testNums.length; j++){
-    if (testAnswers[j] !== userAnswers[j]){
-    	youPassed = false;
-    } else {
-    	continue;
-    }
+// don't change code below this line
+
+var testNums = [123, 2121, 5309, 98765];
+var testAnswers = [321, 1212, 9035, 56789];
+var userAnswers = [];
+var num, ans;
+var pass = true;
+
+
+for (var i = 0; i < 3; i++){
+  num = testNums[i];
+  ans = challengeFunction(num);
+  userAnswers.push(ans);
 }
 
-if (youPassed){
-	console.log("You passed!");
+for (var j = 0; j < 3; j++){
+  if (testAnswers[j] !== userAnswers[j]){
+    pass = false;
+  } else {
+    pass = true;
+  }
+}
+
+if (pass){
+  console.log("You passed!");
 } else {
-	console.log("Try again...");
+  console.log("Challenge currently incomplete");
 }
